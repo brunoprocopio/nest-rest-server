96239b87b94c660d0c6bf5f749b949256eaae10c
brunoprocopio <brunoprocopio59@gmail.com>
2020-05-17 22:01:16 -0300

    feat: format User return

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,7 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema()
+@Schema({
+  toObject: {
+    transform: function (_, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+    }
+  },
+  toJSON: {
+    transform: function (_, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+    }
+  }
+})
 export class User extends Document {
   @Prop()
   name: string;
