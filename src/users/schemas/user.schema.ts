7e8caae47aabaad4fdb0c39ec50c6907d2319d4f
brunoprocopio <brunoprocopio59@gmail.com>
2020-05-17 22:03:02 -0300

    refactor: lint user.schema.ts

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -3,19 +3,19 @@ import { Document } from 'mongoose';
 
 @Schema({
   toObject: {
-    transform: function (_, ret) {
+    transform: function(_, ret) {
       ret.id = ret._id;
       delete ret._id;
       delete ret.__v;
-    }
+    },
   },
   toJSON: {
-    transform: function (_, ret) {
+    transform: function(_, ret) {
       ret.id = ret._id;
       delete ret._id;
       delete ret.__v;
-    }
-  }
+    },
+  },
 })
 export class User extends Document {
   @Prop()
